33e062a47311e30983dc7ef0ddec11eca60877b8
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 04:53:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 
 def reverse_each_word(sentence)
   new_sentence = []
-  word_array = sentence.split.each do |backward_words|
+  sentence.split.each do |backward_words|
     new_sentence << backward_words.reverse
   end
   new_sentence.join(" ")
