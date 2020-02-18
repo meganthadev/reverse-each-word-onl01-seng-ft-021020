2985cd54efc289867da0a41c50ee29fed62241ab
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 04:52:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,6 @@ end
  
   reverse_each_word("Hello there, and how are you?")
   
-  def reverse_each_word(sentence) 
     def reverse_each_word(sentence)
   sentence.split.collect do |backward_words|
     backward_words.reverse
