8398bbe1743b01a2ae78ffcd415f882ee099b684
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 04:39:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
   new_sentence = []
-  word_array = sentence.split.each do
+  word_array = sentence.split.each do |backwards_words|
  
   
