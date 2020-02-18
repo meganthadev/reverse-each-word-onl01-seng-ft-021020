2e88ac860f101f3a2e08ae49fccab0f88e6cefbf
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 04:47:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
   new_sentence = []
-  word_array = sentence.split.each do |backwards_words|
+  word_array = sentence.split.each do |backward_words|
     new_sentence << backward_words.reverse
   end
   new_sentence
